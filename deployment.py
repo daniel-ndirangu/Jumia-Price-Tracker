5c6209b205393848b0e33b1d5e06a3b5f0ede4c6
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-19 16:38:25 +0300

    Update deployment file

--- deployment.py
+++ deployment.py
@@ -1,12 +1,18 @@
 from prefect import flow
+from prefect.schedules import CronSchedule
+
 
 if __name__ == "__main__":
+    schedule = CronSchedule(
+        cron="35 16 * * *",
+        timezone="Africa/Nairobi"
+    )
+
     flow.from_source(
         source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git",
         entrypoint="flow.py:scrape_jumia"
     ).deploy(
-        name = "jumia_scraper",
+        name="jumia_scraper",
         work_pool_name="jumia-managed-pool",
-        cron="35 16 * * *",
-        timezone="Africa/Nairobi"
+        schedule=schedule
     )
