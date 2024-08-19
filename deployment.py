c42870a59723bec0ade1857f756ef2da9b614e17
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-19 14:32:59 +0300

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -2,7 +2,7 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git"
+        source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git",
         entrypoint="flow.py:scrape_jumia"
     ).deploy(
         name = "jumia_scraper",
