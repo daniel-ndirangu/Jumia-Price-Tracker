1b1e884aa56fd9efa019679d09d5e542d05966f8
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-20 17:34:43 +0300

    update schedule

--- create_deployment.py
+++ create_deployment.py
@@ -4,7 +4,7 @@ from prefect.client.schemas.schedules import CronSchedule
 
 if __name__ == "__main__":
     schedule = CronSchedule(
-        cron="10 17 * * *",
+        cron="0 18 * * *",
         timezone="Africa/Nairobi"
     )
 
