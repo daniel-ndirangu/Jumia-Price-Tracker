2c4407507dc81a29d1d54ca40a67b152e13da087
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-21 02:19:58 +0300

    Update file

--- jumia_flow.py
+++ jumia_flow.py
@@ -2,15 +2,29 @@ from prefect import flow, task
 import subprocess  
 
 
-@task
-def run_spider():
-    return subprocess.run(["scrapy", "crawl", "products"])
+# @task
+# def run_spider():
+#     return subprocess.run(["scrapy", "crawl", "products"], capture_output=True)
     
-@flow
-def scrape_jumia():
-    run_spider()
-    
+# @flow
+# def scrape_jumia():
+#     run_spider()
        
+# if __name__ == "__main__":
+#     scrape_jumia()
+
+
+from prefect import flow
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from jumiascraper.spiders.samsung import SamsungSpider
+
+@flow
+def run_scraper():
+    process = CrawlerProcess()
+    process.crawl(SamsungSpider)
+    process.start()
+    
 if __name__ == "__main__":
-    scrape_jumia()
+    run_scraper()
     
