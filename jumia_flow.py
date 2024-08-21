4887978f5b3dbcb50163d5744a43f676222881c3
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-21 22:08:38 +0300

    Update file

--- jumia_flow.py
+++ jumia_flow.py
@@ -1,37 +1,7 @@
-from prefect import flow, task
 import subprocess  
-
-
-# @task
-# def run_spider():
-#     return subprocess.run(["scrapy", "crawl", "products"], capture_output=True)
-    
-# @flow
-# def scrape_jumia():
-#     run_spider()
-       
-# if __name__ == "__main__":
-#     scrape_jumia()
-
-
-from prefect import flow
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from jumiascraper.spiders.samsung import SamsungSpider
-
-
-# @flow
-# def run_scraper():
-    
-#     process = CrawlerProcess()
-#     process.crawl(SamsungSpider)
-#     process.start()
-    
-# if __name__ == "__main__":
-#     run_scraper()
-
-
-import subprocess
 from prefect import task,flow,get_run_logger
 
 
@@ -42,18 +12,22 @@ def run_query():
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
         raise Exception(stderr.decode())
+
+
 @flow
 def run_all_task():
   run_query()
+  
+
   
   
 if __name__ == "__main__":
   run_all_task.from_source(
     source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git",
     entrypoint="jumia_flow.py:run_all_task"
-    ).deploy(
+     ).deploy(
        name="my-first-deployment",
        work_pool_name="my-managed-pool",
-       cron="0 20 * * *")
+        cron="30 22 * * *")
 
     
