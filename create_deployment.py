57f8d83327e8c6cadba0ea90a52ee3de620296c8
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-21 19:29:46 +0300

    Update files

---
+++ create_deployment.py
@@ -0,0 +1,11 @@
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git",
+        entrypoint="jumia_flow.py:run_all_task",
+    ).deploy(
+        name="my-first-deployment",
+        work_pool_name="my-managed-pool",
+        cron="0 20 * * *",
+    )
