288627f62ba89068af2a2b0a31d7e5d673301586
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-22 20:17:52 +0300

    Update Job variables

--- jumia_flow.py
+++ jumia_flow.py
@@ -1,6 +1,5 @@
 import subprocess  
 from prefect import task,flow,get_run_logger
-import os
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
@@ -35,9 +34,7 @@ def run_query():
 @flow
 def run_all_task():
     run_query()
-
-
-  
+    
 
 if __name__ == "__main__":
   run_all_task.from_source(
@@ -46,7 +43,7 @@ if __name__ == "__main__":
      ).deploy(
        name="my-first-deployment",
        work_pool_name="my-work-pool",
-       job_variables={"pip_packages": ["scrapy", "scrapy-playwright", "pymongo", "w3lib", "datetime"]},
+       job_variables={"pip_packages": ["scrapy", "scrapy-playwright", "pymongo", "w3lib", "datetime", "prefect"]},
        cron = "20 20 * * *"
        )
 
