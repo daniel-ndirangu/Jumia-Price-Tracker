eb471b79309118b8c993b3ef591c3c6afc215994
daniel-ndirangu <ndirangudaniel.ds@gmail.com>
2024-08-24 16:44:50 +0300

    Update file

--- jumia_flow.py
+++ jumia_flow.py
@@ -39,17 +39,17 @@ def run_all_task():
     
 
 
-if __name__ == "__main__":
-  run_all_task.from_source(
-    source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git",
-    entrypoint="jumia_flow.py:run_all_task", 
-     ).deploy(
-       name="my-first-deployment",
-       work_pool_name="my-managed-pool",
-       job_variables={
-         "env": 
-           {"EXTRA_PIP_PACKAGES": "scrapy==2.11.2 scrapy-playwright==0.0.40 pymongo==4.8.0 datetime prefect"}
-           },
-       interval = timedelta(hours=8)
-       )
+# if __name__ == "__main__":
+#   run_all_task.from_source(
+#     source="https://github.com/daniel-ndirangu/Jumia-Price-Tracker.git",
+#     entrypoint="jumia_flow.py:run_all_task", 
+#      ).deploy(
+#        name="my-first-deployment",
+#        work_pool_name="my-managed-pool",
+#        job_variables={
+#          "env": 
+#            {"EXTRA_PIP_PACKAGES": "scrapy==2.11.2 scrapy-playwright==0.0.40 pymongo==4.8.0 datetime prefect"}
+#            },
+#        interval = timedelta(hours=8)
+#        )
     
